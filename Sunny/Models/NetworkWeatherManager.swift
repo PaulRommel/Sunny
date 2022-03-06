7ec561df73178ab8bdc7095da7984d01a02205f7
Paul <paulrommel87@gmail.com>
2022-03-06 16:01:34 +0300

    12. App interface update

--- Sunny/Models/NetworkWeatherManager.swift
+++ Sunny/Models/NetworkWeatherManager.swift
@@ -3,27 +3,22 @@
 //  Sunny
 //
 //  Created by Павел Попов on 27.02.2022.
-//  Copyright © 2022 Ivan Akulov. All rights reserved.
 //
 
 import Foundation
 
-protocol NetworkWeatherManagerDelegate: AnyObject {
-    func updateInterface(_: NetworkWeatherManager, with currentWeather: CurrentWeather)
-}
-
 class NetworkWeatherManager {
 
-    weak var delegate: NetworkWeatherManagerDelegate?
+    var onCompletion: ((CurrentWeather) -> Void)?
 
     func fetchCurrentWeather(forCity city: String) {
-        let urlString = "https://api.openweathermap.org/data/2.5/weather?q=\(city)&apikey=\(apiKey)"
+        let urlString = "https://api.openweathermap.org/data/2.5/weather?q=\(city)&apikey=\(apiKey)&units=metric"
         guard let url = URL(string: urlString) else { return }
         let session = URLSession(configuration: .default)
         let task = session.dataTask(with: url) { data, response, error in
             if let data = data {
                 if let currentWeather = self.parseJSON(withData: data) {
-                    self.delegate?.updateInterface(self, with: currentWeather)
+                    self.onCompletion?(currentWeather)
                 }
             }
         }
@@ -44,4 +39,3 @@ class NetworkWeatherManager {
         return nil
     }
 }
-
